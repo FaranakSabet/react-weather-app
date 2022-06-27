caedd8bbb23730c03c9f849497e5bff217ba625b
Faranak Sabet <65681350+FaranakSabet@users.noreply.github.com>
2022-06-27 16:47:28 -0700

    Added Github repo Url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
-function App() {
-  return <div className="App">Weather App </div>
+export default function App() {
+  return (
+    <div className="App">
+     <h1>Weather App</h1> 
+      <footer>
+        This project was coded by Faranak Sabet and is {" "}
+        <a 
+        href="https://github.com/FaranakSabet/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
-export default App;
+
